a56c8ea68ff1e6cf69f1a204a8512bf7a7b8849b
dishitarunwal <dishita462@gmail.com>
2025-02-18 16:06:00 +0530

    changes

--- Dishita/form_actions/request_regis.php
+++ Dishita/form_actions/request_regis.php
@@ -1,24 +1,10 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST" || $_SERVER["REQUEST_METHOD"] == "GET") {
-    $name = htmlspecialchars($_REQUEST['name']);
-    $email = htmlspecialchars($_REQUEST['email']);
-    $age = htmlspecialchars($_REQUEST['age']);
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email format";
-        exit;
-    }
-
-    
-    if (!is_numeric($age) || $age <= 0) {
-        echo "Invalid age";
-        exit;
-    }
-
-    echo "<h2>Registration Details (REQUEST Method):</h2>";
-    echo "Name: $name<br>";
-    echo "Email: $email<br>";
-    echo "Age: $age<br>";
+    echo '<script type="text/javascript">';
+    echo 'window.location.href="home.html";';
+    echo '</script>';
+    exit;
 }
+
 ?>
